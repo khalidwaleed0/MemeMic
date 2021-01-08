09a92de4a377fdd3d5d4ac38a9e3984c6b53e0c0
khaled1000emad <58134995+khaled1000emad@users.noreply.github.com>
2021-01-09 01:03:03 +0200

    Initial Release

---
+++ AppSetup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace MemeMic
+{
+    class AppSetup
+    {
+        public static void createText(string path)
+        {
+            TextWriter writer = new StreamWriter("path.txt");
+            writer.WriteLine(path);
+            writer.Close();
+
+        }
+
+        public static void readText()
+        {
+            TextReader reader = new StreamReader("path.txt");
+
+            //    MessageBox.Show(reader.ReadToEnd());
+            reader.Close();
+        }
+    }
+}
---
+++ EventListener.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EventHook;
+using System.IO;
+
+namespace MemeMic
+{
+
+    class EventListener : Control
+    {
+        public EventHookFactory eventHookFactory = new EventHookFactory();
+        public KeyboardWatcher keyboardWatcher;
+        public MouseWatcher mouseWatcher;
+
+        public void captureKeyboardEvent(TextBox textBox)
+        {
+            keyboardWatcher = eventHookFactory.GetKeyboardWatcher();
+            keyboardWatcher.Start();
+            keyboardWatcher.OnKeyInput += (s, e) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    textBox.Text = e.KeyData.Keyname;
+                }
+                );
+            };
+        }
+        public void captureMouseEvent(TextBox textbox)
+        {
+            mouseWatcher = eventHookFactory.GetMouseWatcher();
+            mouseWatcher.Start();
+
+            mouseWatcher.OnMouseInput += (s, e) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    String mouseEvent = e.Message.ToString();
+                    if (!mouseEvent.Equals("WM_MOUSEMOVE"))
+                    {
+                        textbox.Text = e.Message.ToString();
+                    }
+
+
+                }
+                );
+            };
+        }
+
+
+
+
+
+    }
+
+}
