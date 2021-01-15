07632c3729f07858b8c6c3d68fdf6cbb3f3a82b6
khalidwaleed0 <khalidwaleed0@outlook.com>
2021-01-15 23:50:21 +0200

    Now it fully works with the overlay

--- EventListener.cs
+++ EventListener.cs
@@ -18,51 +18,128 @@ using System.IO;
 
 namespace MemeMic
 {
-
     class EventListener : Control
     {
         public EventHookFactory eventHookFactory = new EventHookFactory();
         public KeyboardWatcher keyboardWatcher;
         public MouseWatcher mouseWatcher;
-
+        private string overlayState = "Hidden";
+        private OverlayWindow overlay = new OverlayWindow();
+        private MemePlayer player = new MemePlayer();
+        private int memeIndex = 0;
         public void captureKeyboardEvent()
         {
             keyboardWatcher = eventHookFactory.GetKeyboardWatcher();
             keyboardWatcher.Start();
             keyboardWatcher.OnKeyInput += (s, e) =>
             {
-                
+                if(e.KeyData.EventType.ToString().Equals("down") && e.KeyData.Keyname.ToString().Equals(AppSetup.readSettingsFile(AppSetup.overlayButtonLine)))
+                    changeOverlayState();
             };
         }
         public void captureMouseEvent()
         {
+            string savedButton = AppSetup.readSettingsFile(AppSetup.overlayButtonLine);
+            string correctMouseData = "";
+            string correctMouseButton = "";
+            string mouseWheelUpData = "7864320";
+            string mouseWheelDownData = "4287102976";
+            get_SavedMouseButton_Data(savedButton, ref correctMouseData, ref correctMouseButton);
             mouseWatcher = eventHookFactory.GetMouseWatcher();
             mouseWatcher.Start();
             mouseWatcher.OnMouseInput += (s, e) =>
             {
-                String mouseEvent = e.Message.ToString();
-                if (!mouseEvent.Equals("WM_MOUSEMOVE"))
+                string mouseInputName = e.Message.ToString();
+                string mouseInputData = e.MouseData.ToString();
+                if(overlayState.Equals("Shown"))
                 {
-                    
+                    if(mouseInputData.Equals(mouseWheelDownData))
+                    {
+                        if(memeIndex != (AppSetup.filteredMemeFiles.Count-1))
+                        {
+                            memeIndex++;
+                            Dispatcher.Invoke(() => {
+                                overlay.unhighlightText(memeIndex - 1);
+                                overlay.highlightText(memeIndex);
+                            });
+                        }                                     
+                    }
+                    else if(mouseInputData.Equals(mouseWheelUpData))
+                    {
+                        if (memeIndex != 0)
+                        {
+                            memeIndex--;
+                            Dispatcher.Invoke(() => {
+                                overlay.unhighlightText(memeIndex + 1);
+                                overlay.highlightText(memeIndex);
+                            });
+                        }
+                    }
                 }
+
+                if (savedButton.Contains("XButton"))
+                {
+                    if (mouseInputData.Equals(correctMouseData) && mouseInputName.Contains("DOWN"))
+                        changeOverlayState();
+                }
+                else if(mouseInputName.Equals(correctMouseButton))
+                        changeOverlayState();
             };
         }
-/*        ~EventListener()
+
+        private void get_SavedMouseButton_Data(string savedButton, ref string correctMouseData, ref string correctMouseButton)
         {
-            keyboardWatcher.Stop();
-            mouseWatcher.Stop();
-            eventHookFactory.Dispose();
-        }*/
+            switch (savedButton)
+            {
+                case "XButton1":
+                    correctMouseData = "65536";
+                    break;
+                case "XButton2":
+                    correctMouseData = "131072";
+                    break;
+                case "Middle":
+                    correctMouseData = "0";
+                    correctMouseButton = "WM_WHEELBUTTONDOWN";
+                    break;
+            }
+        }
+        private void changeOverlayState()
+        {
+            switch (overlayState)
+            {
+                case "Hidden":
+                    Dispatcher.Invoke(() => { 
+                        overlay.Show();
+                        overlay.highlightText(0);
+                    });
+                    overlayState = "Shown";
+                    memeIndex = 0;
+                    break;
+                case "Shown":
+                    player.play(AppSetup.filteredMemeFiles[memeIndex]);
+                    overlayState = "Playing";
+                    break;
+                case "Playing":
+                    player.stop();
+                    Dispatcher.Invoke(() => {
+                        overlay.unhighlightText(memeIndex);
+                        overlay.Hide();
+                    });
+                    overlayState = "Hidden";
+                    break;
+            }
+        }
         public void closeListener()
         {
             keyboardWatcher.Stop();
             mouseWatcher.Stop();
-            //            eventHookFactory.Dispose();
             eventHookFactory.Dispose();
         }
-
-
-
+        ~EventListener()
+        {
+            keyboardWatcher.Stop();
+            mouseWatcher.Stop();
+            eventHookFactory.Dispose();
+        }
     }
-
 }
