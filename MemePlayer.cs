768c937e38c17b34f3d887b826de106b70184145
khalidwaleed0 <khalidwaleed0@outlook.com>
2021-01-16 15:55:12 +0200

    Update MemePlayer.cs

--- MemePlayer.cs
+++ MemePlayer.cs
@@ -4,18 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace MemeMic
 {
     class MemePlayer
     {
         WaveOutEvent player = new WaveOutEvent();
-        public void play()
+        OverlayWindow overlay;
+        public MemePlayer()
         {
-            MediaFoundationReader reader = new MediaFoundationReader(@"F:\memes\Videos\يلعن ميتين أبوكوا.mp4");
+            player.PlaybackStopped += Player_PlaybackStopped;
+        }
+
+        public void play(string path)
+        {
+            MediaFoundationReader reader = new MediaFoundationReader(path);
             player.Init(reader);
             player.Play();
         }
+
+        private void Player_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+
+        }
+
         public void stop()
         {
             player.Dispose();
