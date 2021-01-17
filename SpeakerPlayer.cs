8d70cd103a455203df06e6708be52d6782eb9e75
khalidwaleed0 <khalidwaleed0@outlook.com>
2021-01-18 01:13:32 +0200

    Initial Release

---
+++ SpeakerPlayer.cs
@@ -0,0 +1,28 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemeMic
+{
+    class SpeakerPlayer
+    {
+        WaveOutEvent speakerPlayer = new WaveOutEvent();
+        public SpeakerPlayer()
+        {
+            speakerPlayer.DeviceNumber = 0;                        // -1 is the default playback device
+        }
+        public void play(string path)
+        {
+            MediaFoundationReader reader = new MediaFoundationReader(path);
+            speakerPlayer.Init(reader);
+            speakerPlayer.Play();
+        }
+        public void Stop()
+        {
+            speakerPlayer.Dispose();
+        }
+    }
+}
