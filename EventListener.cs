81813092e7f3de57e872427f374ba2fed6b95880
khalidwaleed0 <khalidwaleed0@outlook.com>
2021-01-19 13:43:36 +0200

    Update EventListener.cs

--- EventListener.cs
+++ EventListener.cs
@@ -44,6 +44,8 @@ namespace MemeMic
             string savedButton = AppSetup.readSettingsFile(AppSetup.overlayButtonLine);
             string correctMouseData = "";
             string correctMouseButton = "";
+            const string mouseWheelUpData = "7864320";
+            const string mouseWheelDownData = "4287102976";
             get_SavedMouseButton_Data(savedButton, ref correctMouseData, ref correctMouseButton);
             mouseWatcher = eventHookFactory.GetMouseWatcher();
             mouseWatcher.Start();
@@ -53,8 +55,6 @@ namespace MemeMic
                 string mouseInputData = e.MouseData.ToString();
                 if(overlayState.Equals("Shown"))
                 {
-                    const string mouseWheelUpData = "7864320";
-                    const string mouseWheelDownData = "4287102976";
                     if (mouseInputData.Equals(mouseWheelDownData))
                         scrollDown();
                     else if (mouseInputData.Equals(mouseWheelUpData))
