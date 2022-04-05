9d0314330c317ea89159523b66a23cbcaebb6106
Khalid Waleed <khalidwaleed0@outlook.com>
2022-04-05 19:58:34 +0200

    Fixed Bug : app exits if there is no internet connection

--- Updater.cs
+++ Updater.cs
@@ -15,39 +15,47 @@ namespace MemeMic
         public async void update()
         {
             WebClient client = new WebClient();
-            string source = client.DownloadString("https://github.com/khalidwaleed0/MemeMic/releases");
-            var config = Configuration.Default;
-            var context = BrowsingContext.New(config);
-            var document = await context.OpenAsync(req => req.Content(source));
-            var element = document.QuerySelector("h1 .Link--primary");
-            string latestReleaseName = element.TextContent.Trim();
-            if (!latestReleaseName.Equals("v1.1"))
+            try
             {
-                Application.Current.Dispatcher.Invoke(() =>{
-                    updateWindow.firstTextBlock.Text = "Downloading latest version...";
-                });
-                var latestReleaseElement = document.QuerySelector(".Box.Box--condensed.mt-3 a");
-                string latestReleaseLink = "https://github.com/" + latestReleaseElement.GetAttribute("href");
-                download(latestReleaseLink);
-                try
+                string source = client.DownloadString("https://github.com/khalidwaleed0/MemeMic/releases");
+                var config = Configuration.Default;
+                var context = BrowsingContext.New(config);
+                var document = await context.OpenAsync(req => req.Content(source));
+                var element = document.QuerySelector("h1 .Link--primary");
+                string latestReleaseName = element.TextContent.Trim();
+                if (!latestReleaseName.Equals("v1.1.1"))
+                {
+                    Application.Current.Dispatcher.Invoke(() => {
+                        updateWindow.firstTextBlock.Text = "Downloading latest version...";
+                    });
+                    var latestReleaseElement = document.QuerySelector(".Box.Box--condensed.mt-3 a");
+                    string latestReleaseLink = "https://github.com/" + latestReleaseElement.GetAttribute("href");
+                    download(latestReleaseLink);
+                    try
+                    {
+                        waitForMemeBox();
+                        System.Diagnostics.Process.Start("update.bat");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        MessageBox.Show("The new installation file is on desktop");
+                    }
+                    client.Dispose();
+                    Environment.Exit(0);
+                }
+                else
                 {
                     waitForMemeBox();
-                    System.Diagnostics.Process.Start("update.bat");
+                    Application.Current.Dispatcher.Invoke(() => { updateWindow.forceClose(); });
+                    client.Dispose();
                 }
-                catch(Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                    MessageBox.Show("The new installation file is on desktop");
-                }
-                client.Dispose();
-                Environment.Exit(0);
             }
-            else
+            catch (Exception ex)
             {
-                waitForMemeBox();
                 Application.Current.Dispatcher.Invoke(() => { updateWindow.forceClose(); });
-                client.Dispose();
             }
+            
         }
         public void download(string latestReleaseLink)
         {
